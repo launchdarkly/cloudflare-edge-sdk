3a824a04f186438ca7d6bd0a11bb17280d365877
Jaz White <jwhite@launchdarkly.com>
2021-08-24 12:49:10 -0400

    [ch119485] Remove node cache in feature store (#6)
    * [ch119485] Apply auto-linting fixes
    
    * [ch119485] Remove node cache in feature store since it is implemented in node sdk

--- cloudflare_feature_store.js
+++ cloudflare_feature_store.js
@@ -1,84 +1,62 @@
-const CachingStoreWrapper = require('launchdarkly-node-server-sdk/caching_store_wrapper')
-const NodeCache = require('node-cache')
-const noop = function() {}
+const CachingStoreWrapper = require('launchdarkly-node-server-sdk/caching_store_wrapper');
+const noop = function () {};
 
-var defaultCacheTTLSeconds = 15
+const defaultCacheTTLSeconds = 15;
 
 const kvStore = function CloudFlareKVFeatureStore(storeNameTest, options) {
-  var ttl = options && options.cacheTTL
+  let ttl = options && options.cacheTTL;
   if (ttl === null || ttl === undefined) {
-    ttl = defaultCacheTTLSeconds
+    ttl = defaultCacheTTLSeconds;
   }
-  return new CachingStoreWrapper(
-    new cfFeatureStoreInternal(storeNameTest, options),
-    ttl,
-  )
-}
+  return new CachingStoreWrapper(new cfFeatureStoreInternal(storeNameTest, options), ttl);
+};
 
 function cfFeatureStoreInternal(storeName, options) {
-  const cache = new NodeCache({ stdTTL: 30 })
-  options = options || {}
-  const store = {}
-  const prefix = options.prefix || ''
-  const fullPrefix = kind => {
-    return prefix ? `${prefix}:${kind}:` : `${kind}:`
-  }
+  options = options || {};
+  const store = {};
 
   store.getInternal = (kind, key, maybeCallback) => {
-    const cb = maybeCallback || noop
-    const findKey = key
-    const kindData = cache.get(kind.namespace)
-    if (kindData == undefined) {
-      storeName.get('featureData').then(item => {
-        const parseData = JSON.parse(item)
-        cache.set('features', parseData['features'])
-        cache.set('segments', parseData['segments'])
-        cb(parseData[kind.namespace][key])
-      })
-    } else {
-      console.log(`data: ${kindData[key]}`)
-      cb(kindData[key])
-    }
-  }
+    const cb = maybeCallback || noop;
+    storeName.get('featureData').then(item => {
+      const parseData = JSON.parse(item);
+      cb(parseData[kind.namespace][key]);
+    });
+  };
 
   store.getAllInternal = (kind, maybeCallback) => {
-    const cb = maybeCallback || noop
-    const kindData = cache.get(kind.namespace)
+    const cb = maybeCallback || noop;
 
-    if (kindData == undefined) {
-      storeName.get('featureData').then(item => {
-        const parseData = JSON.parse(item)
-        cache.set('features', parseData['features'])
-        cache.set('segments', parseData['segments'])
-        cb(parseData[kind.namespace])
-      })
-    } else {
-      cb(kindData)
-    }
-  }
+    storeName.get('featureData').then(item => {
+      const parseData = JSON.parse(item);
+      cb(parseData[kind.namespace]);
+    });
+  };
 
   store.initInternal = async (allData, cb) => {
-    await insertKindAll(allData)
-    cb && cb()
-  }
+    await insertKindAll(allData);
+    cb && cb();
+  };
 
-  store.upsertInternal = (kind, item, cb) => {}
+  store.upsertInternal = (kind, item, cb) => {};
 
   async function insertKindAll(allData) {
-    await storeName.put('featureData', JSON.stringify(allData))
+    await storeName.put('featureData', JSON.stringify(allData));
   }
 
-  store.initializedInternal = async (maybeCallback) => {
-    const cb = maybeCallback || noop
-    (function() { cb && cb(); })();
-  }
+  store.initializedInternal = async maybeCallback => {
+    const cb =
+      maybeCallback ||
+      noop(() => {
+        cb && cb();
+      })();
+  };
 
   // KV Binding is done outside of the application logic.
-  store.close = () => {}
+  store.close = () => {};
 
-  return store
+  return store;
 }
 
 module.exports = {
   CloudFlareKVFeatureStore: kvStore,
-}
+};
