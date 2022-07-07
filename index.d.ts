10b0e574d9985ff74024e3739ad9fc68a0888c29
Christie Williams <xie.williams@gmail.com>
2022-07-07 10:46:08 -0400

    Update type def and test (#22)
    * Update type def and test
    
    * Update launchdarkly-node-server-sdk
    
    * Update test user
    
    * Only test with current LTS versions of node

--- index.d.ts
+++ index.d.ts
@@ -4,7 +4,7 @@
  * This is the API reference for the LaunchDarkly Edge SDK for Cloudflare.
  */
 
-declare module 'launchdarkly-cloudflare-edge-sdk/client' {
+declare module 'launchdarkly-cloudflare-edge-sdk' {
   import { LDEvaluationDetail, LDFeatureStore, LDFlagsState, LDFlagsStateOptions, LDFlagValue, LDLogger, LDOptions, LDUser } from 'launchdarkly-node-server-sdk';
 
   /**
@@ -27,7 +27,7 @@ declare module 'launchdarkly-cloudflare-edge-sdk/client' {
   export function init(
     kvNamespace: KVNamespace,
     sdkKey: string,
-    options: LDCFWorkerOptions,
+    options?: LDCFWorkerOptions,
   ): LDClient;
 
   export interface LDClient {
@@ -74,8 +74,8 @@ declare module 'launchdarkly-cloudflare-edge-sdk/client' {
      *   }
      * ```
      */
-
     waitForInitialization(): Promise<LDClient>;
+
     /**
      * Determines the variation of a feature flag for a user.
      *
@@ -176,48 +176,4 @@ declare module 'launchdarkly-cloudflare-edge-sdk/client' {
      */
     featureStore?: LDFeatureStore | ((options: LDOptions) => LDFeatureStore);
   }
-}
-
-declare module 'launchdarkly-cloudflare-edge-sdk/feature_store' {
-  import { LDFeatureStore, LDLogger, LDOptions } from 'launchdarkly-node-server-sdk';
-
-  /**
-   * Configures a feature store backed by cloudflare KV
-   *
-   * For more details about how and why you can use a persistent feature store, see
-   * the [SDK features guide](https://docs.launchdarkly.com/sdk/features/database-integrations).
-   *
-   * @param kvNamespace
-   *   KV store namespace binding
-   * @param sdkKey
-   *   The client-side SDK key for the environment
-   * @param options
-   *   Configurations parameters for the feature store
-   * @param logger
-   *   A custom logger for warnings and errors. If not specified, it will use whatever the
-   *   SDK's logging configuration is.
-   *
-   * @returns
-   *   A factory function that the SDK will use to create the data store. Put this value into the
-   *   `featureStore` property of [[LDOptions]].
-   */
-  export function CloudflareFeatureStore(
-    kvNamespace: KVNamespace,
-    sdkKey: string,
-    options?: LDCloudflareOptions,
-    logger?: LDLogger | object,
-  ): (config: LDOptions) => LDFeatureStore;
-
-  /**
-   * The standard options supported for the LaunchDarkly Redis integration.
-   */
-  export interface LDCloudflareOptions {
-    /**
-     * The amount of time, in seconds, that recently read or updated items should remain in an
-     * in-memory cache. If it is zero, there will be no in-memory caching.
-     * The default value is 60 seconds.
-     */
-    cacheTTL?: number;
-  }
-
 }
